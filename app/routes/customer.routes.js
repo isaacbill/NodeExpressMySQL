6b032fe9486ae2b9f0ca5a9b95cedba206054210
Mogaka1994 <isaacbill247@gmail.com>
2023-08-22 23:47:40 +0300

    node mysql connection

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
 
   // Create a new Customer
-  app.post("/customer", customers.create);
+  app.post("/customers", customers.create);
 
   // Retrieve all Customers
   app.get("/customers", customers.findAll);
